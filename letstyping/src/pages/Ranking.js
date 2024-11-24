60a6dd710033ff11e1e308b09e386b280c02a317
se0y <gong021200@naver.com>
2024-11-25 02:38:38 +0900

    FEAT : 워들 퀴즈 추가

--- letstyping/src/pages/Ranking.js
+++ letstyping/src/pages/Ranking.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import RankingModal from "../components/ranking/RankingModal";
+import Quiz from "../components/ranking/Quiz";
+import QuizComp from "../components/ranking/QuizComp";
 
-const Ranking = ({ name = "서연" }) => { // 타이핑제목, 링크주소, 유저이름 받아와야 함
+const Ranking = ({ name = "서연" }) => { // 타이핑제목, 링크주소, 유저이름, 유저점수, 키워드, 키워드 설명 받아와야 함
   const [rankingData, setRankingData] = useState([]);
 
   useEffect(() => {
     const data = localStorage.getItem("rankingData");
     const storedData = JSON.parse(data);
 
-    // 로컬스토리지에 데이터가 없으면 기본 mock 데이터를 저장
+    // 로컬스토리지에 데이터가 없으면 기본 데이터 저장
     if (!storedData) {
       const mockRankingData = [
         { name: "미정", score: 542 },
@@ -31,12 +33,17 @@ const Ranking = ({ name = "서연" }) => { // 타이핑제목, 링크주소, 유
 
   
 
-  return <RankingModal 
-    rankingData={rankingData} 
-    title="안중근이 독립 기원하며 쓴 글씨, 15년만에 국내 공개" 
-    link="https://example.com" 
-    name={name}
-   />;
+  return (
+    <>
+      <Quiz /> 
+      <RankingModal
+        rankingData={rankingData}
+        title="안중근이 독립 기원하며 쓴 글씨, 15년만에 국내 공개"
+        link="https://example.com"
+        name={name}
+      />
+    </>
+  );
 };
 
 export default Ranking;
